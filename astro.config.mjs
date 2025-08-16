af38d65e4131753f3de9d24ec2eb9c64343bb0df
Emanuel Jimenez <emanuel.jimenez9123@alumnos.udg.mx>
2025-08-15 20:28:02 -0600

    Refactor astro.config.mjs to include Tailwind CSS and Vercel adapter configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import vercer from "@astrojs/vercel/serverless";
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  output: 'server',
+  adapter: vercel(),
+});
